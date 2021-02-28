4cd3965712ebe808be3512162e95d4feb20a9c92
developert1990 <developert.hong@gmail.com>
2021-02-27 22:45:34 -0700

    add secrets for domain

--- scripts/set-env.sh
+++ scripts/set-env.sh
@@ -4,3 +4,4 @@ echo "RIOT_TOKEN=$RIOT_TOKEN" > .env
 echo "PORT=$PORT" >> .env
 echo "MONGODB_URL=$MONGODB_URL" >> .env
 echo "JWT_SECRET=$JWT_SECRET" >> .env
+echo "COOKIE_DOMAIN_PROD=COOKIE_DOMAIN_PROD" >> .env
